c768974506414137dfe954479fb374938360613c
userhas404d <29389186+userhas404d@users.noreply.github.com>
2020-01-28 12:31:53 -0500

    Updates ci to utilize tardigrade-ci

--- tests/create_sg/_docs/MAIN.md
+++
@@ -1,1 +0,0 @@
-# Create SG Test
--- tests/use_all_variables/main.tf
+++ tests/use_all_variables/main.tf
@@ -23,24 +23,24 @@ module "use_all_variables" {
   vpc_id    = module.vpc.vpc_id
   ingress_rules = [
     {
-      "from_port" = "0",
-      "to_port" = "0",
-      "protocol" = "-1",
+      "from_port"   = "0",
+      "to_port"     = "0",
+      "protocol"    = "-1",
       "cidr_blocks" = ["0.0.0.0/0"]
     }
   ]
 
   egress_rules = [
     {
-      "from_port" = "0",
-      "to_port" = "0",
-      "protocol" = "-1",
+      "from_port"   = "0",
+      "to_port"     = "0",
+      "protocol"    = "-1",
       "cidr_blocks" = ["0.0.0.0/0"]
     }
   ]
 
   revoke_rules_on_delete = true
-  tags      = {
+  tags = {
     environment = "testing"
   }
 }
